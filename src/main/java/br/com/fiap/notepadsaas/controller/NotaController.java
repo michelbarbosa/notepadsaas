d4dc1ce5cb70b730b704d168c93a814993c280b7
logonrm <logonrm@fiap.com.br>
2017-08-15 20:35:12 -0300

    ajustes controller + mod repositorio

--- src/main/java/br/com/fiap/notepadsaas/controller/NotaController.java
+++ src/main/java/br/com/fiap/notepadsaas/controller/NotaController.java
@@ -2,15 +2,14 @@ package br.com.fiap.notepadsaas.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.fiap.notepadsaas.model.Nota;
 import br.com.fiap.notepadsaas.repository.NotaRepository;
 
 @RestController
+@CrossOrigin
+@RequestMapping(value = "/nota")
 public class NotaController {
 	private NotaRepository notaRepository;
 
@@ -18,12 +17,17 @@ public class NotaController {
 		this.notaRepository = notaRepository;
 	}
 	
-	@GetMapping(value = "/")
+	@GetMapping(value = "")
 	public List<Nota> findAll() {
 		return notaRepository.findAll();
 	}
+
+	@GetMapping(value = "/titulo/{titulo}")
+	private List<Nota> findByTitulo(@PathVariable(value = "titulo") String titulo) {
+		return notaRepository.findByTitulo(titulo);
+	}
 	
-	@PostMapping(value = "/")
+	@PostMapping(value = "")
 	public void save(@RequestBody Nota nota) {
 		notaRepository.save(nota);
 	}
--- src/main/java/br/com/fiap/notepadsaas/repository/NotaRepository.java
+++ src/main/java/br/com/fiap/notepadsaas/repository/NotaRepository.java
@@ -4,5 +4,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import br.com.fiap.notepadsaas.model.Nota;
 
+import java.util.List;
+
 public interface NotaRepository extends MongoRepository<Nota, String> {
+    List<Nota> findByTitulo(String titulo);
 }
